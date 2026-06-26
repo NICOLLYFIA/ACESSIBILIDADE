6a17eef2f258c7707ff0a8f8c55ebcd83aefdd59
NICOLLYFIA <YOHANNA.FURLAN@ESCOLA.PR.GOV.BR>
2026-06-26 15:42:35 +0000

    alteraçao 1

---
+++ script.js
@@ -0,0 +1,15 @@
+let tamanhoFonte = 18;
+
+function aumentarFonte()(
+    tamanhoFonte +=2;
+    document.getElementById(`texto´). style.fontSize = tamamanhoFonte + 'px';
+{
+function diminuirContraste()(
+    tamanhoFonte +=2;
+    document.getElementById(`texto´). style.fontSize = tamamanhoFonte + 'px';
+    }
+    {
+function altoContraste()(
+    tamanhoFonte +=2;
+    document.getElementById(`texto´). style.fontSize = tamamanhoFonte + 'px';
+    }
